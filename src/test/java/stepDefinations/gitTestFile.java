b6a31be33fd0fd86f73df056aa2db6ce7fe7aaa3
Anju <gothi.anju@gmail.com>
2023-07-16 10:04:29 -0700

    Ankit's Assignment

--- src/test/java/stepDefinations/gitTestFile.java
+++ src/test/java/stepDefinations/gitTestFile.java
@@ -4,8 +4,10 @@ public class gitTestFile {
 	
 	public static void testMethod() {
 		
-		System.out.println("Git test file");
-		System.out.println("Git test file2");
+		System.out.println("Your Assignment:");
+		System.out.println("1. Write a java prg to fine 2nd largest integer in a array of integers");
+		System.out.println("2. Reverse a String without changing order Ex.Ankit is learning skills Output:tiknA si gninrael  slliks");
+		System.out.println(": )");
 	}
 
 }
